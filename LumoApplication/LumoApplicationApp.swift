ac4569d0498e10b3004868a9d8c0a12ab8b9b5c2
Quang Tu <quangtu2301@gmail.com>
2025-11-06 14:41:33 +1100

    Initial Commit

---
+++ LumoApplication/LumoApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumoApplicationApp.swift
+//  LumoApplication
+//
+//  Created by Quang Tu Nguyen on 6/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct LumoApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
