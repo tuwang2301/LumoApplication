ebfc27ce320f5ace16191200fa8607d302a682e9
Naman015 <55298452+Naman015@users.noreply.github.com>
2025-11-10 22:04:57 +1100

    '}' typo fix

--- LumoApplication/ContentView.swift
+++ LumoApplication/ContentView.swift
@@ -11,30 +11,31 @@ struct ContentView: View {
     @EnvironmentObject var appState: AppState
     
     var body: some View {
-            NavigationStack(path: $appState.path) {
-                LandingView()
-                    .navigationDestination(for: String.self) { value in
-                        switch value {
-                      case "explore":
-                          ExploreView()
-                              .toolbarBackground(.black, for: .navigationBar)
-                              .toolbarColorScheme(.dark, for: .navigationBar)
-                      case "confirmation":
-                          ConfirmEmotionsView()
-                      case "visualise":
+        NavigationStack(path: $appState.path) {
+            LandingView()
+                .navigationDestination(for: String.self) { value in
+                    switch value {
+                    case "explore":
+                        ExploreView()
+                            .toolbarBackground(.black, for: .navigationBar)
+                            .toolbarColorScheme(.dark, for: .navigationBar)
+                    case "confirmation":
+                        ConfirmEmotionsView()
+                    case "visualise":
                         VisualiseView(selectedEmotions: appState.selectedEmotions)
-                        case "summary":
-                            SummaryView()
-                        default:
-                            EmptyView()
-                        }
-            }
-            .opacity(appState.hasSeenHook ? 1 : 0)
-            .animation(.easeInOut(duration: 0.5), value: appState.hasSeenHook)
-            .onAppear {
-                // When the app's main view appears,
-                // request notification permission.
-                NotificationManager.shared.requestPermission()
-            }
+//                    case "summary":
+//                        ConfirmEmotionsView()
+                    default:
+                        EmptyView()
+                    }
+                }
+                .opacity(appState.hasSeenHook ? 1 : 0)
+                .animation(.easeInOut(duration: 0.5), value: appState.hasSeenHook)
+                .onAppear {
+                    // When the app's main view appears,
+                    // request notification permission.
+                    NotificationManager.shared.requestPermission()
+                }
         }
     }
+}
